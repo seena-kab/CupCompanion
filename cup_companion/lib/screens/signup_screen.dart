0b149113608261b0e300f7b018fbe56aef759172
A-Jaime <100460041+A-Jaime@users.noreply.github.com>
2024-09-10 19:02:55 -0700

    added password visibility

--- cup_companion/lib/screens/signup_screen.dart
+++ cup_companion/lib/screens/signup_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:cup_companion/services/auth_services.dart';  // Correct path  // Import your AuthService class
+import 'package:cup_companion/services/auth_services.dart'; // Correct path  // Import your AuthService class
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -17,8 +17,9 @@ class SignUpScreenState extends State<SignUpScreen> {
 
   bool _isLoading = false;
   String? _errorMessage;
+  bool isPasswordVisible = false; // Password visibility tracker
 
-  //Method to validate email format
+  // Method to validate email format
   bool _isValidEmail(String email) {
     final RegExp emailRegex = RegExp(
       r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9]+\.[a-zA-Z]+$",
@@ -27,10 +28,10 @@ class SignUpScreenState extends State<SignUpScreen> {
     return emailRegex.hasMatch(email);
   }
 
-  //Method to show a simple alert dialog
+  // Method to show a simple alert dialog
   void _showDialog(String message) {
     showDialog(
-      context: context, 
+      context: context,
       builder: (context) => AlertDialog(
         title: const Text('Error'),
         content: Text(message),
@@ -50,47 +51,47 @@ class SignUpScreenState extends State<SignUpScreen> {
       _errorMessage = null;
     });
 
-  //Trim and validate the input
-  final String email = _emailController.text.trim();
-  final String password = _passwordController.text.trim();
-  final String username = _usernameController.text.trim();
-  final String mobileNumber = _mobileNumberController.text.trim();
+    // Trim and validate the input
+    final String email = _emailController.text.trim();
+    final String password = _passwordController.text.trim();
+    final String username = _usernameController.text.trim();
+    final String mobileNumber = _mobileNumberController.text.trim();
 
-  if (email.isEmpty || password.isEmpty || username.isEmpty || mobileNumber.isEmpty) {
-    _showDialog('All fields are required');
-    setState(() {
-      _isLoading = false;
-    });
-    return;
-  }
-
-  if (!_isValidEmail(email)) {
-    _showDialog('Please enter a valid email address');
-    setState(() {
-      _isLoading = false;
-    });
-    return;
-  }
-
-  if (password.length < 6) {
-    _showDialog('Password must be at least 6 characters');
-    setState(() {
-      _isLoading = false;
-    });
-    return;
-  }
-
-  try {
-    // Call the AuthService to create a user with email and password
-    await _authService.createUser(
-      email,
-      password,
-    );
-
-    // Navigate to a different screen (e.g., home page) after successful sign-up
-    if (mounted) {
-    Navigator.pushReplacementNamed(context, '/signin');
+    if (email.isEmpty || password.isEmpty || username.isEmpty || mobileNumber.isEmpty) {
+      _showDialog('All fields are required');
+      setState(() {
+        _isLoading = false;
+      });
+      return;
     }
+
+    if (!_isValidEmail(email)) {
+      _showDialog('Please enter a valid email address');
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
+
+    if (password.length < 6) {
+      _showDialog('Password must be at least 6 characters');
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
+
+    try {
+      // Call the AuthService to create a user with email and password
+      await _authService.createUser(
+        email,
+        password,
+      );
+
+      // Navigate to a different screen (e.g., home page) after successful sign-up
+      if (mounted) {
+        Navigator.pushReplacementNamed(context, '/signin');
+      }
     } catch (e) {
       // Catch and display any errors during the sign-up process
       setState(() {
@@ -98,9 +99,9 @@ class SignUpScreenState extends State<SignUpScreen> {
       });
     } finally {
       if (mounted) {
-      setState(() {
-        _isLoading = false;
-      });
+        setState(() {
+          _isLoading = false;
+        });
       }
     }
   }
@@ -131,8 +132,10 @@ class SignUpScreenState extends State<SignUpScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/images/logo.png',
-            height: 200,),
+            Image.asset(
+              'assets/images/logo.png',
+              height: 200,
+            ),
             const SizedBox(height: 30.0),
             TextField(
               controller: _usernameController,
@@ -161,12 +164,23 @@ class SignUpScreenState extends State<SignUpScreen> {
             const SizedBox(height: 16.0),
             TextField(
               controller: _passwordController,
-              decoration: const InputDecoration(
+              obscureText: !isPasswordVisible, // Controls password visibility
+              decoration: InputDecoration(
                 labelText: 'Password',
-                border: OutlineInputBorder(),
-                suffixIcon: Icon(Icons.visibility),
+                border: const OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    isPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      isPasswordVisible = !isPasswordVisible; // Toggle visibility
+                    });
+                  },
+                ),
               ),
-              obscureText: true,
             ),
             const SizedBox(height: 24.0),
             _isLoading
