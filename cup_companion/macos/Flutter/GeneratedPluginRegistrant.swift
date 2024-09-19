f020755cde216c52abb9973afa01d4440dbb9157
romebalti <rbaltazar542000@gmail.com>
2024-09-18 20:57:55 -0700

    romans local changes

--- cup_companion/macos/Flutter/GeneratedPluginRegistrant.swift
+++ cup_companion/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,8 +11,8 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_database
-import location
 import firebase_storage
+import location
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
@@ -25,6 +25,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
